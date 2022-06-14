6546ead3eb0f0a48da5127e8061143565e508239
Steffen Vogel <post@steffenvogel.de>
2022-06-14 12:18:56 +0200

    update Protobuf definitions

---
+++ pkg/pb/vand.go
@@ -0,0 +1,20 @@
+package pb
+
+import (
+	"fmt"
+	"io"
+	"time"
+
+	"github.com/stv0g/vand/pkg/types"
+)
+
+func (s *StateUpdatePoint) Dump(wr io.Writer) {
+	f := types.Flatten(s, ".")
+	for k, v := range f {
+		fmt.Fprintf(wr, "%s = %v\n", k, v)
+	}
+}
+
+func (ts *Timestamp) Time() time.Time {
+	return time.Unix(int64(ts.Seconds), int64(ts.Nanoseconds))
+}
