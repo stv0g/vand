9cf52436bc2c681a857226198d9b028f35fe26cc
Steffen Vogel <post@steffenvogel.de>
2022-06-14 12:18:08 +0200

    add state store

--- pkg/store/store.go
+++ pkg/store/store.go
@@ -1,24 +1,81 @@
 package store
 
-import "github.com/stv0g/vand/pkg/pb"
+import (
+	"log"
+	"sync"
 
-type State struct {
-	Solar       *pb.SolarState
-	Battery     *pb.BatteryState
-	Gps         *pb.GpsState
-	Car         *pb.CarOBD2State
-	Environment *pb.EnvironmentState
-	Modem       *pb.ModemState
-}
+	pmqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/golang/protobuf/proto"
+	"github.com/imdario/mergo"
+	"github.com/stv0g/vand/pkg/mqtt"
+	"github.com/stv0g/vand/pkg/pb"
+	"github.com/stv0g/vand/pkg/types"
+)
 
 type Store struct {
-	State State
+	client *mqtt.Client
+
+	subs     map[chan *pb.StateUpdatePoint]struct{}
+	subsLock sync.RWMutex
+
+	State pb.StateUpdatePoint
 }
 
-func (s *Store) Update(msg *pb.StateUpdateMessage) error {
-	return nil
+func NewStore(client *mqtt.Client, topic string) (*Store, error) {
+	s := &Store{}
+
+	client.Subscribe(topic, 2, s.messageHandler)
+
+	return s, nil
 }
 
-func (s *Store) GetValues() map[string]string {
-	return map[string]string{}
+func (s *Store) Update(sup *pb.StateUpdatePoint) error {
+	s.Notify(sup)
+
+	return mergo.Merge(&s.State, sup, mergo.WithOverride)
+}
+
+func (s *Store) messageHandler(client pmqtt.Client, msg pmqtt.Message) {
+	var sup pb.StateUpdatePoint
+
+	if err := proto.Unmarshal(msg.Payload(), &sup); err != nil {
+		log.Printf("Failed to unmarshal payload: %s", err)
+	}
+
+	if err := s.Update(&sup); err != nil {
+		log.Printf("Failed to update store: %s", err)
+	}
+}
+
+func (s *Store) Flatten(sep string) map[string]interface{} {
+	return types.Flatten(&s.State, sep)
+}
+
+func (s *Store) Notify(sup *pb.StateUpdatePoint) {
+	s.subsLock.RLock()
+	defer s.subsLock.RUnlock()
+
+	for ch := range s.subs {
+		ch <- sup
+	}
+}
+
+func (s *Store) Subscribe() (chan *pb.StateUpdatePoint, error) {
+	ch := make(chan *pb.StateUpdatePoint)
+
+	s.subsLock.Lock()
+	defer s.subsLock.Unlock()
+
+	s.subs[ch] = struct{}{}
+
+	return nil, nil
+}
+
+func (s *Store) Unsubscribe(ch chan *pb.StateUpdatePoint) {
+	s.subsLock.Lock()
+	defer s.subsLock.Unlock()
+
+	delete(s.subs, ch)
+
+	close(ch)
 }
