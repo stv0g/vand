f898414bd3406331995ee76b9b72cbb39f134719
Steffen Vogel <post@steffenvogel.de>
2024-08-20 09:01:09 +0200

    nix: Build frontend
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- flake.nix
+++ flake.nix
@@ -20,43 +20,71 @@
         pkgs = nixpkgs.legacyPackages.${system};
       in
       rec {
-        packages.vand = pkgs.buildGoModule {
-          name = "vand";
-          src = ./.;
-          vendorHash = "sha256-dPNv9uFGbAk9Ul3YQ2woaifwez18O6plVDfd67grP+c=";
-          buildInputs =
-            with pkgs;
-            [
-              nodejs_18
-              protobuf
-              protoc-gen-go
-            ]
-            ++ (
-              if stdenv.isLinux then
+        packages = {
+          vand =
+            let
+              frontend = pkgs.buildNpmPackage {
+                name = "vand-frontend";
+                src = ./frontend;
+                npmDepsHash = "sha256-5tXDAYY07aVnrCn7QIiPJrv9Fu+yG+Pyy9iysli6S/o=";
+                installPhase = ''
+                  mkdir $out
+                  cp -r build/* $out/
+                '';
+              };
+            in
+            pkgs.buildGoModule {
+              name = "vand";
+              src = ./.;
+              vendorHash = "sha256-dPNv9uFGbAk9Ul3YQ2woaifwez18O6plVDfd67grP+c=";
+
+              preBuild = ''
+                rm -rf frontend/build
+                cp -r ${frontend}/ frontend/build/
+              '';
+
+              postInstall = ''
+                mv $out/bin/cmd $out/bin/vand
+              '';
+
+              tags = [
+                "embed_frontend"
+                "virtual"
+              ];
+
+              buildInputs =
+                with pkgs;
                 [
-                  vulkan-headers
-                  libxkbcommon
-                  wayland
-                  libGL
-                  pkg-config
-                  xorg.libX11
-                  xorg.libXcursor
-                  xorg.libXfixes
+                  protobuf
+                  protoc-gen-go
                 ]
-              else if stdenv.isDarwin then
-                with darwin.apple_sdk_11_0;
-                [
-                  MacOSX-SDK
-                  frameworks.Foundation
-                  frameworks.Metal
-                  frameworks.QuartzCore
-                  frameworks.AppKit
-                ]
-              else
-                [ ]
-            )
-            ++ (if stdenv.isLinux then with pkgs; [ libxkbcommon ] else [ ]);
-          doCheck = false;
+                ++ (
+                  if stdenv.isLinux then
+                    [
+                      vulkan-headers
+                      libxkbcommon
+                      wayland
+                      libGL
+                      pkg-config
+                      xorg.libX11
+                      xorg.libXcursor
+                      xorg.libXfixes
+                    ]
+                  else if stdenv.isDarwin then
+                    with darwin.apple_sdk_11_0;
+                    [
+                      MacOSX-SDK
+                      frameworks.Foundation
+                      frameworks.Metal
+                      frameworks.QuartzCore
+                      frameworks.AppKit
+                    ]
+                  else
+                    [ ]
+                )
+                ++ (if stdenv.isLinux then with pkgs; [ libxkbcommon ] else [ ]);
+              doCheck = false;
+            };
         };
 
         devShell = pkgs.mkShell {
@@ -65,6 +93,7 @@
             reuse
             gnumake
             rsync
+            nodejs_18
           ];
 
           inputsFrom = [ packages.vand ];
