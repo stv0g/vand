fa054cd3dce173ab382daf89d4f6d1127470e12f
Steffen Vogel <post@steffenvogel.de>
2025-05-21 23:14:17 +0200

    chore: Update Nix flake input and NodeJS version
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- flake.nix
+++ flake.nix
@@ -24,67 +24,60 @@
           default = packages.backend;
 
           frontend = pkgs.buildNpmPackage {
-                name = "vand-frontend";
-                src = ./frontend;
-                npmDepsHash = "sha256-5tXDAYY07aVnrCn7QIiPJrv9Fu+yG+Pyy9iysli6S/o=";
-                installPhase = ''
-                  mkdir $out
-                  cp -r build/* $out/
-                '';
-              };
+            name = "vand-frontend";
+            src = ./frontend;
+            npmDepsHash = "sha256-RG76cZJUem8k8rQmMPA3nyRP0gcEOIi4XlqzJqI8bDs=";
+            installPhase = ''
+              mkdir $out
+              cp -r build/* $out/
+            '';
+          };
 
           backend = pkgs.buildGo124Module {
-              name = "vand-backend";
-              src = ./.;
-              vendorHash = "sha256-85x6oOsfu2kQXaRhNKPRb5ZI8SKEsTAKD/5p0aFOM4E=";
+            name = "vand-backend";
+            src = ./.;
+            vendorHash = "sha256-uYi0XjRaa4OdG0nNvAjmqWN5VtGJ1oD+HktjWXB/fkI=";
 
-              preBuild = ''
-                rm -rf frontend/build
-                cp -r ${packages.frontend}/ frontend/build/
+            preBuild = ''
+              rm -rf frontend/build
+              cp -r ${packages.frontend}/ frontend/build/
 
-                echo $PATH
-                pkg-config --version
-              '';
+              echo $PATH
+              pkg-config --version
+            '';
 
-              postInstall = ''
-                mv $out/bin/cmd $out/bin/vand
-              '';
+            postInstall = ''
+              mv $out/bin/cmd $out/bin/vand
+            '';
 
-              tags = [
-                "embed_frontend"
-                "virtual"
-              ];
+            tags = [
+              "embed_frontend"
+              "virtual"
+            ];
 
-              nativeBuildInputs = with pkgs; [
-                pkg-config
-                protobuf
-                protoc-gen-go
-              ];
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+              protobuf
+              protoc-gen-go
+            ];
 
-              buildInputs = with pkgs; if stdenv.isLinux then
-                    [
-                      vulkan-headers
-                      libxkbcommon
-                      wayland
-                      libGL
-                      xorg.libX11
-                      xorg.libXcursor
-                      xorg.libXfixes
-                    ]
-                  else if stdenv.isDarwin then
-                    with darwin.apple_sdk_11_0;
-                    [
-                      MacOSX-SDK
-                      frameworks.Foundation
-                      frameworks.Metal
-                      frameworks.QuartzCore
-                      frameworks.AppKit
-                    ]
-                  else
-                    [ ];
+            buildInputs =
+              with pkgs;
+              if stdenv.isLinux then
+                [
+                  vulkan-headers
+                  libxkbcommon
+                  wayland
+                  libGL
+                  xorg.libX11
+                  xorg.libXcursor
+                  xorg.libXfixes
+                ]
+              else
+                [ ];
 
-              doCheck = false;
-            };
+            doCheck = false;
+          };
         };
 
         devShell = pkgs.mkShell {
@@ -93,7 +86,7 @@
             reuse
             gnumake
             rsync
-            nodejs_18
+            nodejs_24
           ];
 
           inputsFrom = with packages; [
